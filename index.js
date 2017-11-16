5d031fbe6b754930b410b2284416e88a99d3965f
Candu <canduecho@foxmail.com>
2017-11-16 17:51:57 +0800

    test data

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var time = require('time');
 exports.handler = (event, context, callback) => {
 	var currentTime = new time.Date();
 	currentTime.setTimezone("America/Los_Angeles");
-    var result = 'The time in Los Angeles is '+ currentTime.toString();
+    var result = 'The time is '+ currentTime.toString();
     var response = {
         statusCode: 200,
         body: JSON.stringify(result)
