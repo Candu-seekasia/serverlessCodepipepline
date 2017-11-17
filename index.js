eee06711347d469ff5eceb05fc8e0a12a32f6eda
Candu <canduecho@foxmail.com>
2017-11-17 17:50:21 +0800

    connect db

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
 console.log('CodePipeline sample');
 
 var time = require('time');
+ var sql = require("mssql");
 
 exports.handler = (event, context, callback) => {
+	 var config = {
+        user: 'sqlCFSWebAppHK',
+        password: 'O7nRxZLrWUTT8nlI2yYB',
+        server: '10.90.10.76', 
+        database: 'JobsDB_System' 
+    };
+
 	var currentTime = new time.Date();
 	currentTime.setTimezone("America/Los_Angeles");
     var result = 'The time1 is '+ currentTime.toString();
@@ -11,6 +19,26 @@ exports.handler = (event, context, callback) => {
         statusCode: 200,
         body: JSON.stringify(result)
     };
-    console.log("response data: " + JSON.stringify(response))
-	callback(null, response);
+
+       // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from [Country]', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            //res.send(recordset);
+            console.log("response data: " + JSON.stringify(response))
+			callback(null, recordset);
+            
+        });
+    });
+    
 };
