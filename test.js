2f454516dd1ce10e897b8640dd93a9e458a91675
candu <candu@seekasia.com>
2018-02-08 10:11:09 +0800

    change for local test

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 'use strict';
 
 exports.handler = (event, context, callback) => {
-	
+    
+    console.log("isproduction",process.env.ISPRODUCTION+"");
+    console.log("debug",process.env.DEBUGE+""); 
     var response = {
         statusCode: 200,
-        body: JSON.stringify("test")
+        body: JSON.stringify("test "+event.name)
     };
     callback(null, response);
 
